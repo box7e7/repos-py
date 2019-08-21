ed4f86b4b7534093c0c20518b53ba94970e55ead
box7e7 <parislight@live.com>
2019-08-21 11:33:52 -0400

    test_5.py

--- test_5.py
+++ test_5.py
@@ -10,6 +10,16 @@ def remove_duplicate_words(s):
 
 	var=[]
 	var=s.split()
+	result=[]
+	
+	for i in range(0,len(var)):
+		if var[i]!="null":
+
+			ins=indexes(var,var[i])
+			for j in range(1,len(ins)):
+				var[ins[j]]="null"
+	
 	for i in var:
-		print(i)
-	return var
+		if i!="null":
+			result.append(i)
+	return result
