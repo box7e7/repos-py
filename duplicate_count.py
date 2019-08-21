b52e53b03d90107348490817a2634a3bc2d76ede
box7e7 <parislight@live.com>
2019-08-21 15:16:24 -0400

    duplicate_count.py

---
+++ duplicate_count.py
@@ -0,0 +1,32 @@
+def duplicate_count(text):
+    list2=[ ]
+    list1=[]
+    Result=[]
+    str=text
+    print(len(str))
+    for i in range(0,len(str)):
+        list1.append(str[i].lower())
+               
+
+    for i in list1:
+        if i not in Result:
+            Result.append(i)
+        
+    print(Result)  
+
+    for i in range(0,len(Result)):
+    
+        k=0
+        for j in list1:
+            if Result[i]==j:
+                k+=1
+        list2.append(k)        
+            
+      
+    count=0
+    for i in list2:
+        if i>1:
+            count+=1
+    return count       
+        
+     
