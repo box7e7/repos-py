dfba32ad7702e777878db9762234998c970048af
box7e7 <parislight@live.com>
2019-08-23 12:05:02 -0400

    number_of_pairs.py

--- number_of_pairs.py
+++ number_of_pairs.py
@@ -1,16 +1,12 @@
 def number_of_pairs(gloves):
-    k=0
+    
     count=0
     list1=[]
     for i in gloves:
         if i not in list1:
             list1.append(i)
     for i in list1:
-        k=0
-        for j in gloves:
-            if i==j:
-                k=k+1
-        count+=int(k/2)        
+        count+=int(gloves.count(i)/2)
                 
             
     return  count 
