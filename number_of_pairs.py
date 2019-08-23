0d85bed4c2b06a562b8dab204818fe33c5cce2be
box7e7 <54365246+box7e7@users.noreply.github.com>
2019-08-23 12:12:53 -0400

    Update number_of_pairs.py

--- number_of_pairs.py
+++ number_of_pairs.py
@@ -5,5 +5,5 @@ def number_of_pairs(gloves):
     for i in gloves:
         if i not in list1:
             list1.append(i)
-            count+=gloves.count(i)/?2
+            count+=gloves.count(i)//2
     return  count 
