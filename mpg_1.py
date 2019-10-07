08ba86661dbc0c2da970107885456779d0bb848f
box7e7 <54365246+box7e7@users.noreply.github.com>
2019-10-07 14:30:09 -0400

    Update mpg_1.py

--- mpg_1.py
+++ mpg_1.py
@@ -4,6 +4,7 @@ import seaborn as sns
 
 df=sns.load_dataset('mpg')
 serie1=df.name[df.origin=='europe'].str.split().str[0]
+arr1=df.name[df.origin=='europe'].str.split().str[0].unique() #list of unique European car model names, listed in an array.
 dict1={'volkswagen': 'Germany', 'peugeot': 'France', 'audi': 'Germany', 'saab': 'Sweeden', 'bmw': 'Germany', 'opel': 'Germany', 'fiat': 'Italy', 'volvo': 'Sweeden', 'renault': 'France', 'vw': 'Germany', 'mercedes-benz': 'Germany', 'mercedes': 'Germany', 'vokswagen': 'Germany', 'triumph': 'England'}
 for i in serie1.index:
 	serie1[i]=dict1[serie1[i]]
